31bdfc5884b420d0b3ce7c1a024931924775163d
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2015-08-10 02:24:47 +0300

    Added the Apache 2.0 License.

--- src/main/scala/robots/protocol/exclusion/html/Page.scala
+++ src/main/scala/robots/protocol/exclusion/html/Page.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package robots.protocol.exclusion.html
 
 import java.net.URL
@@ -9,8 +25,6 @@ import scala.xml.Node
  * Representation of a HTML page at a given url.
  *
  * @param url Location of the page
- *
- * @author Andrei Heidelbacher
  */
 final class Page private (val url: URL, content: Node) {
   private def getMetaTags(robotName: String): Option[Seq[Tag]] = {
--- src/main/scala/robots/protocol/exclusion/html/Tag.scala
+++ src/main/scala/robots/protocol/exclusion/html/Tag.scala
@@ -1,11 +1,25 @@
+/*
+ * Copyright 2015 Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package robots.protocol.exclusion.html
 
 /**
  * Abstract representation of possible robots meta-tags in html documents. If a
  * new tag is added, it should be appended to the
  * [[robots.protocol.exclusion.html.Tag.supportedTags]] sequence.
- *
- * @author Andrei Heidelbacher
  */
 sealed abstract class Tag {
   /**
